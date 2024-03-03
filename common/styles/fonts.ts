e8e5541276d4b628816c9050f0907dd0c5050baa
gabrieljuninho <gabrieltobing789@gmail.com>
2024-03-03 19:46:56 +0700

    depen: setup husky

--- common/styles/fonts.ts
+++ common/styles/fonts.ts
@@ -2,5 +2,5 @@ import localFont from "next/font/local";
 
 export const fontHeading = localFont({
   src: "../../assets/fonts/CalSans-SemiBold.woff2",
-  variable: "--font-heading",
+  variable: "--font-heading"
 });
